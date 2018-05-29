7026ea74fa62b34a8b5939b59e67f6f391df7ea3
Yongfei Hu <yongfei.hu@everbridge.com>
2018-05-29 16:32:39 +0800

    update

--- main.tf
+++ main.tf
@@ -1,6 +1,4 @@
 provider "aws" {
-        access_key = "${var.access_key}"
-        secret_key = "${var.secret_key}"
 	region = "us-east-1"
 }
 
@@ -8,6 +6,6 @@ resource "aws_instance" "yongfeiuall" {
 	ami = "ami-1853ac65"
 	instance_type = "t2.micro"
 	tags{
-	Name = "simple single server"
+	Name = "integration with jenkins"
 	}
 }
---
+++ variables.tf
@@ -0,0 +1,7 @@
+variable "access_key" {
+	default = "AKIAJGAAYPEGRIZQEAKQ"
+}
+
+variable "secret_key" {
+	default = "KKtPzxmRc5ePPjRXQTj70DbDjXFulORyWSU6BVRQ"
+}
